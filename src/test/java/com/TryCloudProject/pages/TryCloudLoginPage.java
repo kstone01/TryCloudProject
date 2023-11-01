c08624815c5cd9c6f0bd003213c7fa77dfb80a63
kstone01 <kaystone730@gmail.com>
2023-11-01 14:37:11 -0400

    created TryCloudLoginPage with elements located

---
+++ src/test/java/com/TryCloudProject/pages/TryCloudLoginPage.java
@@ -0,0 +1,25 @@
+package com.TryCloudProject.pages;
+
+import com.TryCloudProject.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TryCloudLoginPage {
+
+    public TryCloudLoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id = "user")
+    public WebElement usernameInput;
+
+    @FindBy(id = "password")
+    public WebElement passwordInput;
+
+    @FindBy(id = "submit-form")
+    public WebElement loginButton;
+
+
+}
