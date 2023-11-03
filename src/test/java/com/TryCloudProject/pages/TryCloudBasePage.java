e62e10c0292ddd5176ad381bd764b2797a0ed70b
kstone01 <kaystone730@gmail.com>
2023-11-02 23:13:20 -0400

    Base Page created

---
+++ src/test/java/com/TryCloudProject/pages/TryCloudBasePage.java
@@ -0,0 +1,32 @@
+package com.TryCloudProject.pages;
+
+import com.TryCloudProject.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TryCloudBasePage {
+
+    public TryCloudBasePage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "(//div[@class='header-left']//a[@href='/index.php/apps/files/'])[1]")
+    public WebElement filesButton;
+
+
+    @FindBy(xpath = "(//table//tbody//td)[1]")
+    public WebElement itemToSelect;
+
+    @FindBy(xpath = "//a[@data-action='menu']")
+    public WebElement threeDotsMenu;
+
+    @FindBy(xpath = "(//li)[55]//span[.='Delete folder']")
+    public  WebElement deleteFolderButton;
+
+    @FindBy(xpath = "//td/span[@class='info']")
+    public WebElement fileCount;
+
+}
